b8fe5350e148091815aae81b08dc4ed90968af88
Brian Bechtel <brianrbechtel@gmail.com>
2018-04-18 15:00:10 -0500

    Reworded stream field from 'myTuples' to 'tuples'

--- src/gammastream/TupleStream.java
+++ src/gammastream/TupleStream.java
@@ -33,11 +33,11 @@ import gammasupport.BMap;
 public class TupleStream implements Stream<Tuple> {
     
     private TableSchema schema;
-    private Stream<Tuple> myTuples;
+    private Stream<Tuple> tuples;
     
     public TupleStream(TableSchema schema, Stream<Tuple> stream) {
         this.schema = Objects.requireNonNull(schema);
-        this.myTuples = Objects.requireNonNull(stream);
+        this.tuples = Objects.requireNonNull(stream);
     }
     
     public static TupleStream readtable(String filename) {
@@ -51,7 +51,7 @@ public class TupleStream implements Stream<Tuple> {
     
     public TupleStream gammaJoin(TupleStream other, String joinkey1, String joinkey2) {
         // if we are parallel, turn on parallel for the other stream
-        if (myTuples.isParallel()) {
+        if (this.tuples.isParallel()) {
             other = other.parallel();
         }
         BMap bitMap = new BMap();
@@ -72,17 +72,17 @@ public class TupleStream implements Stream<Tuple> {
     
     public TupleStream hjoin(TupleStream other, String joinkey1, String joinkey2) {
         // if we are parallel, turn on parallel for the other stream
-        if (myTuples.isParallel()) {
+        if (this.tuples.isParallel()) {
             other = other.parallel();
         }
         // collectors.groupingby uses a HashMap which is what we want
-        Map<String, List<Tuple>> map = myTuples.collect(Collectors.groupingBy(t -> t.get(joinkey1)));
+        Map<String, List<Tuple>> map = this.tuples.collect(Collectors.groupingBy(t -> t.get(joinkey1)));
         
         // create joinTable Schema
-        TableSchema joinSchema = this.getSchema().crossProduct(other.getSchema());
+        TableSchema joinSchema = this.schema.crossProduct(other.schema);
         
         // use other.myTuples directly to avoid piling on wrappers of TupleStream
-        return new TupleStream(joinSchema, other.myTuples.filter(t2 -> map.containsKey(t2.get(joinkey2)))
+        return new TupleStream(joinSchema, other.tuples.filter(t2 -> map.containsKey(t2.get(joinkey2)))
                                                          .flatMap(t2 -> map.get(t2.get(joinkey2)).stream()
                                                                            .map(t1 -> new Tuple(joinSchema)
                                                                                           .setValues(t1)
@@ -94,153 +94,153 @@ public class TupleStream implements Stream<Tuple> {
 
     @Override
     public void close() {
-        myTuples.close();
+        tuples.close();
     }
 
     @Override
     public boolean isParallel() {
-        return myTuples.isParallel();
+        return tuples.isParallel();
     }
 
     @Override
     public Iterator<Tuple> iterator() {
-        return myTuples.iterator();
+        return tuples.iterator();
     }
 
     @Override
     public TupleStream onClose(Runnable arg0) {
-        return new TupleStream(schema, myTuples.onClose(arg0));
+        return new TupleStream(schema, tuples.onClose(arg0));
     }
 
     @Override
     public TupleStream parallel() {
-        return new TupleStream(schema, myTuples.parallel());
+        return new TupleStream(schema, tuples.parallel());
     }
 
     @Override
     public TupleStream sequential() {
-        return new TupleStream(schema, myTuples.sequential());
+        return new TupleStream(schema, tuples.sequential());
     }
 
     @Override
     public Spliterator<Tuple> spliterator() {
-        return myTuples.spliterator();
+        return tuples.spliterator();
     }
 
     @Override
     public TupleStream unordered() {
-        return new TupleStream(schema, myTuples.unordered());
+        return new TupleStream(schema, tuples.unordered());
     }
 
     @Override
     public boolean allMatch(Predicate<? super Tuple> arg0) {
-        return myTuples.allMatch(arg0);
+        return tuples.allMatch(arg0);
     }
 
     @Override
     public boolean anyMatch(Predicate<? super Tuple> arg0) {
-        return myTuples.anyMatch(arg0);
+        return tuples.anyMatch(arg0);
     }
 
     @Override
     public <R, A> R collect(Collector<? super Tuple, A, R> arg0) {
-        return myTuples.collect(arg0);
+        return tuples.collect(arg0);
     }
 
     @Override
     public <R> R collect(Supplier<R> arg0, BiConsumer<R, ? super Tuple> arg1,
                          BiConsumer<R, R> arg2) {
-        return myTuples.collect(arg0, arg1, arg2);
+        return tuples.collect(arg0, arg1, arg2);
     }
 
     @Override
     public long count() {
-        return myTuples.count();
+        return tuples.count();
     }
 
     @Override
     public TupleStream distinct() {
-        return new TupleStream(schema, myTuples.distinct());
+        return new TupleStream(schema, tuples.distinct());
     }
 
     @Override
     public TupleStream filter(Predicate<? super Tuple> arg0) {
-        return new TupleStream(schema, myTuples.filter(arg0));
+        return new TupleStream(schema, tuples.filter(arg0));
     }
 
     @Override
     public Optional<Tuple> findAny() {
-        return myTuples.findAny();
+        return tuples.findAny();
     }
 
     @Override
     public Optional<Tuple> findFirst() {
-        return myTuples.findFirst();
+        return tuples.findFirst();
     }
 
     @Override
     public <R> Stream<R> flatMap(Function<? super Tuple, ? extends Stream<? extends R>> arg0) {
-        return myTuples.flatMap(arg0);
+        return tuples.flatMap(arg0);
     }
 
     @Override
     public DoubleStream flatMapToDouble(Function<? super Tuple, ? extends DoubleStream> arg0) {
-        return myTuples.flatMapToDouble(arg0);
+        return tuples.flatMapToDouble(arg0);
     }
 
     @Override
     public IntStream flatMapToInt(Function<? super Tuple, ? extends IntStream> arg0) {
-        return myTuples.flatMapToInt(arg0);
+        return tuples.flatMapToInt(arg0);
     }
 
     @Override
     public LongStream flatMapToLong(Function<? super Tuple, ? extends LongStream> arg0) {
-        return myTuples.flatMapToLong(arg0);
+        return tuples.flatMapToLong(arg0);
     }
 
     @Override
     public void forEach(Consumer<? super Tuple> arg0) {
-        myTuples.forEach(arg0);
+        tuples.forEach(arg0);
     }
 
     @Override
     public void forEachOrdered(Consumer<? super Tuple> arg0) {
-        myTuples.forEachOrdered(arg0);
+        tuples.forEachOrdered(arg0);
     }
 
     @Override
     public TupleStream limit(long arg0) {
-        return new TupleStream(schema, myTuples.limit(arg0));
+        return new TupleStream(schema, tuples.limit(arg0));
     }
 
     @Override
     public <R> Stream<R> map(Function<? super Tuple, ? extends R> arg0) {
-        return myTuples.map(arg0);
+        return tuples.map(arg0);
     }
 
     @Override
     public DoubleStream mapToDouble(ToDoubleFunction<? super Tuple> arg0) {
-        return myTuples.mapToDouble(arg0);
+        return tuples.mapToDouble(arg0);
     }
 
     @Override
     public IntStream mapToInt(ToIntFunction<? super Tuple> arg0) {
-        return myTuples.mapToInt(arg0);
+        return tuples.mapToInt(arg0);
     }
 
     @Override
     public LongStream mapToLong(ToLongFunction<? super Tuple> arg0) {
-        return myTuples.mapToLong(arg0);
+        return tuples.mapToLong(arg0);
     }
 
     @Override
     public Optional<Tuple> max(Comparator<? super Tuple> arg0) {
-        return myTuples.max(arg0);
+        return tuples.max(arg0);
     }
 
     @Override
     public Optional<Tuple> min(Comparator<? super Tuple> arg0) {
-        return myTuples.min(arg0);
+        return tuples.min(arg0);
     }
 
     @Override
@@ -250,47 +250,47 @@ public class TupleStream implements Stream<Tuple> {
 
     @Override
     public TupleStream peek(Consumer<? super Tuple> arg0) {
-        return new TupleStream(schema, myTuples.peek(arg0));
+        return new TupleStream(schema, tuples.peek(arg0));
     }
 
     @Override
     public Optional<Tuple> reduce(BinaryOperator<Tuple> arg0) {
-        return myTuples.reduce(arg0);
+        return tuples.reduce(arg0);
     }
 
     @Override
     public Tuple reduce(Tuple arg0, BinaryOperator<Tuple> arg1) {
-        return myTuples.reduce(arg0, arg1);
+        return tuples.reduce(arg0, arg1);
     }
 
     @Override
     public <U> U reduce(U arg0, BiFunction<U, ? super Tuple, U> arg1,
                         BinaryOperator<U> arg2) {
-        return myTuples.reduce(arg0, arg1, arg2);
+        return tuples.reduce(arg0, arg1, arg2);
     }
 
     @Override
     public TupleStream skip(long arg0) {
-        return new TupleStream(schema, myTuples.skip(arg0));
+        return new TupleStream(schema, tuples.skip(arg0));
     }
 
     @Override
     public TupleStream sorted() {
-        return new TupleStream(schema, myTuples.sorted());
+        return new TupleStream(schema, tuples.sorted());
     }
 
     @Override
     public Stream<Tuple> sorted(Comparator<? super Tuple> arg0) {
-        return new TupleStream(schema, myTuples.sorted(arg0));
+        return new TupleStream(schema, tuples.sorted(arg0));
     }
 
     @Override
     public Object[] toArray() {
-        return myTuples.toArray();
+        return tuples.toArray();
     }
 
     @Override
     public <A> A[] toArray(IntFunction<A[]> arg0) {
-        return myTuples.toArray(arg0);
+        return tuples.toArray(arg0);
     }
 }
